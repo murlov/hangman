04cebbdd130acdd111b1bcfb37fc0e4ec6f92326
murlov <amolovv@gmail.com>
2025-08-14 21:53:58 +0300

    printing gallows

--- hangman/src/main/java/group/Game.java
+++ hangman/src/main/java/group/Game.java
@@ -1,24 +1,42 @@
 package group;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game {
     public void startGame () throws FileNotFoundException {
         String alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
-        int remainingAttempts;
 
         Dictionary dictionary = new Dictionary();
         dictionary.readWords();
 
         GameInterface gameInterface = new GameInterface();
-        int choice = gameInterface.Menu();
+        gameInterface.printMenu();
+        int choice = gameInterface.getUserChoice();
 
         while (choice != 2) {
             MysteryWord mysteryWord = dictionary.getRandomWord();
             mysteryWord.setHiddenVersionOfWord();
-            System.out.println(mysteryWord.getHiddenVersionOfWord() + "/" + mysteryWord.getNameOfWord());
 
-            choice = gameInterface.Menu();
+            List<Character> usedLetters = new ArrayList<>();
+            int remainingAttempts = 6;
+
+            while (remainingAttempts != 0) {
+                gameInterface.printGallows(remainingAttempts);
+                remainingAttempts -= 1;
+            }
+
+            if (remainingAttempts <= 0) {
+                gameInterface.printGallows(0);
+                gameInterface.printBadResult();
+            } else {
+                gameInterface.printGoodResult();
+            }
+
+
+            gameInterface.printMenu();
+            choice = gameInterface.getUserChoice();
         }
     }
 }
--- hangman/src/main/java/group/GameInterface.java
+++ hangman/src/main/java/group/GameInterface.java
@@ -4,15 +4,17 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GameInterface {
-    public int Menu() {
+
+    public void printMenu() {
+        System.out.println("Меню:\n1.Старт\n2.Выход");
+    }
+
+    public int getUserChoice() {
 
         int number = 0;
+        Scanner scanner = new Scanner(System.in);
 
         do {
-            System.out.println("Меню:\n1.Старт\n2.Выход");
-
-            Scanner scanner = new Scanner(System.in);
-
             try {
                 number = scanner.nextInt();
                 if (number != 1 && number != 2) {
@@ -26,7 +28,85 @@ public class GameInterface {
 
         } while (number != 1 && number != 2);
 
-
         return number;
+    }
+
+    public void printGallows (int remainingAttempts) {
+        switch (remainingAttempts) {
+            case 0:
+                System.out.println( "  +---+\n" +
+                                    "  |   |\n" +
+                                    "  O   |\n" +
+                                    " /|\\  |\n" +
+                                    " / \\  |\n" +
+                                    "      |\n" +
+                                    "=======\n");
+                break;
+            case 1:
+                System.out.println( "  +---+\n" +
+                                    "  |   |\n" +
+                                    "  O   |\n" +
+                                    " /|\\  |\n" +
+                                    " /    |\n" +
+                                    "      |\n" +
+                                    "=======\n");
+                break;
+            case 2:
+                System.out.println( "  +---+\n" +
+                                    "  |   |\n" +
+                                    "  O   |\n" +
+                                    " /|\\  |\n" +
+                                    "      |\n" +
+                                    "      |\n" +
+                                    "=======\n");
+                break;
+            case 3:
+                System.out.println( "  +---+\n" +
+                                    "  |   |\n" +
+                                    "  O   |\n" +
+                                    " /|   |\n" +
+                                    "      |\n" +
+                                    "      |\n" +
+                                    "=======\n");
+                break;
+            case 4:
+                System.out.println( "  +---+\n" +
+                                    "  |   |\n" +
+                                    "  O   |\n" +
+                                    "  |   |\n" +
+                                    "      |\n" +
+                                    "      |\n" +
+                                    "=======\n");
+                break;
+            case 5:
+                System.out.println( "  +---+\n" +
+                                    "  |   |\n" +
+                                    "  O   |\n" +
+                                    "      |\n" +
+                                    "      |\n" +
+                                    "      |\n" +
+                                    "=======\n");
+                break;
+            case 6:
+                System.out.println( "  +---+\n" +
+                                    "  |   |\n" +
+                                    "      |\n" +
+                                    "      |\n" +
+                                    "      |\n" +
+                                    "      |\n" +
+                                    "=======\n");
+                break;
+            default:
+                System.out.println("Ошибка! Не удалось вывести рисунок виселицы!");
+                break;
+        }
+    }
+
+    public void printBadResult () {
+        System.out.println("Коньки отбросили! Попытай удачу ещё раз!\n\n");
+    }
+
+    public void printGoodResult () {
+        System.out.println("Новый День Рождения! Попытай удачу ещё раз!\n\n");
     }
 }
